28734df0ad096cd2f66444e8194f1da2874b8ca0
Zialectical <chris@zialectical.com>
2024-07-07 16:36:12 -0700

    adding latest budget react app changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { AppProvider } from './context/AppContext';
 import Budget from './components/Budget';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
-
 const App = () => {
     return (
         <AppProvider>
